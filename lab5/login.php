82f8b14cd4b756a788fdf30043ebbdc52c95dd8d
s0188223 <loved4248@gmail.com>
2025-03-03 22:50:36 +0300

    add password_verify

--- lab5/login.php
+++ lab5/login.php
@@ -32,12 +32,18 @@ function password_check($login, $password, $db) {
     $stmt = $db->prepare("SELECT password FROM users WHERE login = ?");
     $stmt->execute([$login]);
     $passw = $stmt->fetchColumn();
+
+    if($passw===false){
+      return false;
+    }
+
+    return password_verify($password, $passw);
   } 
   catch (PDOException $e){
     print('Error : ' . $e->getMessage());
-    exit();
+    return false;
   }
-  return $passw == $password;
+  
 }
 
 
