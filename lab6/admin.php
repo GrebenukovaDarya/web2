5f2d6e7ce469c53c9c51742476ccc086d9b8fa25
s0188223 <loved4248@gmail.com>
2025-03-24 10:07:16 +0300

    Update admin.php

--- lab6/admin.php
+++ lab6/admin.php
@@ -1,20 +1,63 @@
 <?php
 if ($_SERVER['REQUEST_METHOD'] == 'GET') {
 
+  $user = 'u68607';
+  $pass = '7232008';
+  $db = new PDO('mysql:host=localhost;dbname=u68607', $user, $pass,
+    [PDO::ATTR_PERSISTENT => true, PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION]);
+
+    function isValid($login, $db) {
+      $check = false;
+      try{
+        $stmt = $db->prepare("SELECT login FROM users WHERE role='admin'");
+        $stmt->execute();
+
+        while ($row = $stmt->fetch(PDO::FETCH_OBJ)) {
+          if($login == $row->login){
+            $check=true;
+          }
+        }
+      } 
+      catch (PDOException $e){
+        print('Error : ' . $e->getMessage());
+        exit();
+      }
+      return $check;
+    }
+    
+    function password_check($login, $password, $db) {
+      $passw;
+      try{
+        $stmt = $db->prepare("SELECT password FROM users WHERE login = ?");
+        $stmt->execute([$login]);
+        $passw = $stmt->fetchColumn();
+        if($passw===false){
+          return false;
+        }
+        return password_verify($password, $passw);
+      } 
+      catch (PDOException $e){
+        print('Error : ' . $e->getMessage());
+        return false;
+      }
+      
+    }
+
   
-if (empty($_SERVER['PHP_AUTH_USER']) ||
-    empty($_SERVER['PHP_AUTH_PW']) ||
-    $_SERVER['PHP_AUTH_USER'] != 'admin' ||
-    md5($_SERVER['PHP_AUTH_PW']) != md5('123')) {
-  header('HTTP/1.1 401 Unanthorized');
-  header('WWW-Authenticate: Basic realm="My site"');
-  print('<h1>401 Требуется авторизация</h1>');
-  exit();
-}
+  if (empty($_SERVER['PHP_AUTH_USER']) ||
+      empty($_SERVER['PHP_AUTH_PW']) ||
+      !is_valid($_SERVER['PHP_AUTH_USER']) ||
+      !password_check($_SERVER['PHP_AUTH_USER'], $_SERVER['PHP_AUTH_PW']), $db) {
 
-print('<div class="">Вы успешно авторизовались и видите защищенные паролем данные.<div>');
+    header('HTTP/1.1 401 Unanthorized');
+    header('WWW-Authenticate: Basic realm="My site"');
+    print('<h1>401 Требуется авторизация</h1>');
+    exit();
+  }
 
-include('table.php');
+  print('<div class="">Вы успешно авторизовались и видите защищенные паролем данные.<div>');
+
+  include('table.php');
 }
 else {
 
