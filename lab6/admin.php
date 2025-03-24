a292e692b6bd9bee2e0335f893dc94909d0a7e72
s0188223 <loved4248@gmail.com>
2025-03-24 10:10:07 +0300

    Update admin.php

--- lab6/admin.php
+++ lab6/admin.php
@@ -50,7 +50,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'GET') {
   
   if (empty($_SERVER['PHP_AUTH_USER']) ||
       empty($_SERVER['PHP_AUTH_PW']) ||
-      !is_valid($_SERVER['PHP_AUTH_USER']) ||
+      !isValid($_SERVER['PHP_AUTH_USER']) ||
       !password_check($_SERVER['PHP_AUTH_USER'], $_SERVER['PHP_AUTH_PW'], $db)) {
 
     header('HTTP/1.1 401 Unanthorized');
