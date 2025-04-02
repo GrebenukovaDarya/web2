ae3159975430d889512c7c03109e6a76d7c0a33e
s0188223 <loved4248@gmail.com>
2025-04-02 16:32:55 +0300

    commit

--- lab6/admin.php
+++ lab6/admin.php
@@ -1,6 +1,6 @@
 <?php
 
-require_once 'func.php';
+require_once '/query.php';
 
 function isValid($login, $db) {
   $check = false;
---
+++ lab6/func/query.php
@@ -0,0 +1,28 @@
+<?php
+
+global $db;
+$user = 'u68607';
+$pass = '7232008';
+$db = new PDO('mysql:host=localhost;dbname=u68607', $user, $pass,
+    [PDO::ATTR_PERSISTENT => true, PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION]);
+
+
+function language_stats($db){
+    $rows = array();
+    try {
+        $stmt = $db->prepare("SELECT lang_name, count(id) AS stat FROM user_lang JOIN prog_lang USING (id_lang) GROUP BY id_lang");
+        $stmt->execute();
+        while($row = $stmt->fetch(PDO::FETCH_OBJ)){
+            $rows[] = "<tr><td>$row->lang_name</td><td>$row->stat</td></tr>";
+        }
+    }
+    catch (PDOException $e){
+        print('ERROR : ' . $e->getMessage());
+        exit();
+    }
+    return $rows;
+}
+
+
+
+?>
